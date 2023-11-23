27e01f506a77dca40f330e58e4a599cebb52f0b7
strawberrysa <sapark0418@gmail.com>
2023-11-23 19:43:57 +0900

    스토어랑푸터완성

--- src/page/Router.jsx
+++ src/page/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import LoginPage from './LoginPage';
+import Shop from './Shop';
 
 function Router() {
   return (
@@ -9,6 +10,8 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/shop" element={<Shop />} />
+        {/* <Route path="/shop/:productid" element={<Shop />} /> */}
       </Routes>
     </BrowserRouter>
   );
