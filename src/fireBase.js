4a065b3af1686a89e5cfbc845f35a093eb35df7a
strawberrysa <sapark0418@gmail.com>
2023-11-23 14:17:26 +0900

    로그인 페이지완성

--- src/fireBase.js
+++ src/fireBase.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "REACT_APP_FB_API_KEY",
-  authDomain: "REACT_APP_FB_AUTH_DOMAIN",
-  projectId: "REACT_APP_PROJECT_ID",
-  storageBucket: "REACT_APP_STORAGE_BUCKET",
-  messagingSenderId: "REACT_APP_MESSAGING_SENDER_ID",
-  appId: "REACT_APP_APP_ID"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTO_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+// REACT_APP_FIREBASE_API_KEY="AIzaSyAuaSJda88E6A1q4Dg2p1tHUttOzZD8PjM"
+// REACT_APP_AUTO_DOMAIN="petlogin-ce0f2.firebaseapp.com"
+// REACT_APP_PROJECT_ID="petlogin-ce0f2"
+// REACT_APP_STORAGE_BUCKET="petlogin-ce0f2.appspot.com"
+// REACT_APP_MESSAGING_SENDER_ID="223184423440"
+// REACT_APP_APP_ID="1:223184423440:web:9346538a1c405b84d0d895"
--- src/page/Router.jsx
+++ src/page/Router.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Home'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import LoginPage from './LoginPage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
